8e66543077b93be6cb58374bb04f8282c3a4938e
Stefan Hagen <stefan@hagen.link>
2021-05-08 11:42:59 +0200

    Removed stray double quote from module docstring

--- cyclonedx_lint/cli.py
+++ cyclonedx_lint/cli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # pylint: disable=expression-not-assigned,line-too-long
-""""Visit folder tree with CycloneDX documents, validate the latter, and generate reports."""
+"""Visit folder tree with CycloneDX documents, validate the latter, and generate reports."""
 import os
 import sys
 
