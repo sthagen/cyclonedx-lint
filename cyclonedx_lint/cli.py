8487d384da5a5483593108f9b8b51cebd4bed108
Stefan Hagen <stefan@hagen.link>
2022-12-27 13:06:40 +0100

    Some modified emptiness

--- cyclonedx_lint/cli.py
+++ cyclonedx_lint/cli.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 """Visit folder tree with CycloneDX documents, validate the latter, and generate reports."""
 import sys
+from typing import no_type_check
 
 import cyclonedx_lint.lint as cyclonedx_lint
 
 
+@no_type_check
 def main(argv=None):
     """Process the job."""
     argv = sys.argv[1:] if argv is None else argv
-    cyclonedx_lint.main(argv)
+    cyclonedx_lint.parse(argv)
--- cyclonedx_lint/lint.py
+++ cyclonedx_lint/lint.py
@@ -1,2 +1,6 @@
-def parse():  # type: ignore
+from typing import no_type_check
+
+
+@no_type_check
+def parse(argv=None):
     return NotImplemented
